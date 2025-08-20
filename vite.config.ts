fa579666f1eef1b6d638c4d8465b98243822f46e
Arnesh Roychowdhury <84434238+ArneshRC@users.noreply.github.com>
2025-08-21 03:58:20 +0530

    chore: enable configurable base URL

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
+const BASE_URL = process.env.VITE_BASE_URL;
+
 export default defineConfig({
 	plugins: [tailwindcss(), solid()],
+  base: BASE_URL
 });
